c14a6ca874e0a4789a0888bb2e8370302a35b128
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2024-12-02 14:57:11 +0700

    connect to mongodb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,32 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+const MONGODB_URI =
+  'mongodb+srv://aassiimmaa:cew1RS8C7kMsVi10@cluster0.xhpom2z.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const DATABASE_NAME = 'trello-andev'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+//Khởi tạo một đối tượng trelloDatabaseInstance ban đầu là null (vì chưa connect)
+let trelloDatabaseInstance = null
+
+//Khởi tạo một đối tượng mongoClientInstance để connect tới mongoDB
+const mongoClientInstance = new MongoClient(MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+export const CONNECT_DB = async () => {
+  // Gọi kết nối tới MongoDB Atlas với URI đã khai báo trong thân của mongoClientInstance
+  await mongoClientInstance.connect()
+
+  //Kết nối thành công thì lấy ra Database theo tên và gán nược nó lại vào biến trelloDatabaseInstance ở trên
+  trelloDatabaseInstance = mongoClientInstance.db(DATABASE_NAME)
+}
+
+//Function GET_DB (không async) này có nhiệm vụ export ra TrelloDatabaseInstance sau khi đã connect thành công tới MongoDB để chúng ta sử dụng ở nhiều nơi khác nhau trong code.
+// Lưu ý phải đảm bảo chỉ luôn gọi GET_DB sau khi đã kết nối thành công tới MongoDB
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database first!')
+  return trelloDatabaseInstance
+}
