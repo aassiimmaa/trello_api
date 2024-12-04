aaed72bf65eaf1f541b545e034fbb2d7689eb723
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2024-12-04 11:46:08 +0700

    code validation, validate data using joi

---
+++ src/validations/boardValidation.js
@@ -0,0 +1,26 @@
+import Joi from 'joi'
+import { StatusCodes } from 'http-status-codes'
+
+const createNew = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    title: Joi.string().required().min(3).max(50).trim().strict().messages({
+      'any.required': 'Title is required',
+      'string.empty': 'Title is not allowed to be empty',
+      'string.min': 'Title must be at least 3 characters long',
+      'string.max': 'Title must be at most 50 characters long',
+      'string.trim': 'Title must not have leading or trailing spaces'
+    }),
+    description: Joi.string().required().min(3).max(256).trim().strict()
+  })
+  try {
+    await correctCondition.validateAsync(req.body, { abortEarly: false })
+    // next()
+    res.status(StatusCodes.CREATED).json({ message: 'API create new board' })
+  } catch (error) {
+    res.status(StatusCodes.UNPROCESSABLE_ENTITY).json({
+      errors: new Error(error).message
+    })
+  }
+}
+
+export const boardValidation = { createNew }
