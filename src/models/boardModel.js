66dc5676a81f074838586f6398cb5c9cc26c69f9
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2024-12-18 10:54:34 +0700

    create new board Api

--- src/models/boardModel.js
+++ src/models/boardModel.js
@@ -15,10 +15,28 @@ const BOARD_COLLECTION_SCHEMA = Joi.object({
   _destroy: Joi.boolean().default(false)
 })
 
+const validateBeforeCreate = async (data) => {
+  return await BOARD_COLLECTION_SCHEMA.validateAsync(data, { abortEarly: false })
+}
+
 const createNew = async data => {
   try {
-    const createdBoard = await GET_DB().collection(BOARD_COLLECTION_NAME).insertOne(data)
+    const validData = await validateBeforeCreate(data)
+    const createdBoard = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .insertOne(validData)
     return createdBoard
+  } catch (error) {
+    throw new Error(error)
+  }
+}
+
+const findOneById = async id => {
+  try {
+    const result = await GET_DB()
+      .collection(BOARD_COLLECTION_NAME)
+      .findOne({ _id: id })
+    return result
   } catch (error) {
     throw new Error(error)
   }
@@ -27,5 +45,6 @@ const createNew = async data => {
 export const boardModel = {
   BOARD_COLLECTION_NAME,
   BOARD_COLLECTION_SCHEMA,
-  createNew
+  createNew,
+  findOneById
 }
