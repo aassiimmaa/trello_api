ce0b03779282b8b3ad83f3961ea67aa7c4101a4c
aassiimmaa <nguyenthanhanvp2690@gmail.com>
2025-02-11 16:00:01 +0700

    complete cards in the same column

--- src/validations/columnValidation.js
+++ src/validations/columnValidation.js
@@ -5,7 +5,10 @@ import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
 
 const createNew = async (req, res, next) => {
   const correctCondition = Joi.object({
-    boardId: Joi.string().required().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE),
+    boardId: Joi.string()
+      .required()
+      .pattern(OBJECT_ID_RULE)
+      .message(OBJECT_ID_RULE_MESSAGE),
     title: Joi.string().required().min(3).max(50).trim().strict()
   })
   try {
@@ -21,4 +24,30 @@ const createNew = async (req, res, next) => {
   }
 }
 
-export const columnValidation = { createNew }
+const update = async (req, res, next) => {
+  const correctCondition = Joi.object({
+    boardId: Joi.string()
+      .pattern(OBJECT_ID_RULE)
+      .message(OBJECT_ID_RULE_MESSAGE),
+    title: Joi.string().min(3).max(50).trim().strict(),
+    cardOrderIds: Joi.array().items(
+      Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)
+    )
+  })
+  try {
+    await correctCondition.validateAsync(req.body, {
+      abortEarly: false,
+      allowUnknown: true
+    })
+    next()
+  } catch (error) {
+    const errorMessage = new Error(error.message)
+    const customError = new ApiError(
+      StatusCodes.UNPROCESSABLE_ENTITY,
+      errorMessage
+    )
+    next(customError)
+  }
+}
+
+export const columnValidation = { createNew, update }
